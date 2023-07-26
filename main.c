6b8865d024e8cd9f109dcbe71d028f030b78a3c5
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-07-26 13:41:16 -0700

    main file

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * main - main file
+ * @ac: Number of arguments passed
+ * @argv: arrays of pointers to the argument
+ *
+ * Return: 0 on success
+ */
 int main(int ac __attribute__((unused)), char *argv[])
 {
 	hsh(argv[0]);
